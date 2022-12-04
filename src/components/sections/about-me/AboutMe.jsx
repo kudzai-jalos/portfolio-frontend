cc20e92fa23b7f4ce77a7aca45dac7981b0eee39
unknown <kudzaijalos@gmail.com>
2022-12-04 10:08:57 +0200

    Fix grammar in about me section

--- src/components/sections/about-me/AboutMe.jsx
+++ src/components/sections/about-me/AboutMe.jsx
@@ -7,12 +7,12 @@ const AboutMe = (props) => {
       <div className={classes["content"]}>
         <h2>About me</h2>
         <p>
-          I am an extremely inspired web developer. I wrote my first lines of code
-          in 2016. Since then, I have learnt many new things and my love for
+          I am very inspired web developer. I wrote my first lines of code
+          in 2016. Since then, I have learnt many new technologies and my love for
           programming has only grown. From a young age, I have always believed I
-          can do anything I put my mind to and that has fueled me to this very day
-          to when writing code. I was immedietly intrigued by software
-          development when it was first introduced to me. I simply love
+          could do anything I put my mind to and that has fueled me to this very day
+          to aim to be the best at anything I do. I was immediately intrigued by software
+          development when it was first introduced to me. I simply enjoy
           solving problems.
         </p>
       </div>
